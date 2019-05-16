70929ba2fa6e3f325613972e2e84d1cafb637ad7
sduwal <sduwal@uno.edu>
2019-05-16 02:14:29 -0500

    small change in productionrb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true #set to false by default
   config.assets.digest = true 
-  config.assets.precompile = %w(*.css, *.js)
+  config.assets.precompile = %w(style.css main.js)
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
