4955e82f8167ee94bcc0d6ba70393a47e2671869
sduwal <sduwal@uno.edu>
2019-05-20 22:35:30 -0500

    Fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+    user=User.where(:id=>1).first
+    raise StandardError, "Project not found." if user.nil?
+        
+    csv_text = File.read(Rails.root.join('lib', 'seeds', 'link.csv'))
+
+    csv= CSV.parse(csv_text,
+            :headers=>true,
+            :encoding =>"ISO-8859-1")
+    csv.each do |row|
+        link_name=row['link']
+        description= row['description']
+
+    unless Photo.exists?(:link=>link_name)
+        Photo.create!(:link=>link_name,
+            :description => description,
+            :user=>user)
+    end
+end
+ 
